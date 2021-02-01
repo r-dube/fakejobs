9655b3f338345575eadae17f6bacabd511f79ab3
r-dube <57359768+r-dube@users.noreply.github.com>
2021-02-01 07:25:13 -0800

    code to generate processed text from raw data file; gensim generates embeddings from the processed text

--- scripts/fj_gensim_embedding.py
+++ scripts/fj_gensim_embedding.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+# For gensim
 from pprint import pprint as print
 from gensim.models.fasttext import FastText as FT_gensim
 from gensim.test.utils import datapath
 
-# Set file names for train and test data
-corpus_file = "../data/fake_job_postings.csv"
+# Set file names
+corpus_file = ".gensim-cache/fj_processed.txt"
+# corpus_file = ".gensim-cache/fj_small_processed.txt"
+save_file = ".gensim-cache/gensim.mod"
 
+# Initialize the model
 model = FT_gensim(size=32)
 
 # build the vocabulary
@@ -17,19 +21,20 @@ model.train(
     corpus_file=corpus_file, epochs=model.epochs,
     total_examples=model.corpus_count, total_words=model.corpus_total_words
 )
-
 print(model)
 
-
 # saving a model trained via Gensim's fastText implementation
-save_file = "gensim.mod"
 model.save(save_file, separately=[])
 
+# run some basic tests on the model
 print ("job" in model.wv.vocab)
 print ("salary" in model.wv.vocab)
 print ("learn" in model.wv.vocab)
 
+# print vector representaion
 print (model["job"])
 
+# test similarity
 print(model.similarity("job", "salary"))
 print(model.similarity("job", "learn"))
+print(model.similarity("job", "the"))

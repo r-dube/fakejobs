5747dd1f8147e56191ba8a0e7507f3350db9df54
r-dube <57359768+r-dube@users.noreply.github.com>
2021-04-05 06:22:05 -0700

    moved presentation to different repo

--- presentations/shellcmd/demo-make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-latexmk -xelatex -output-directory=.latex-cache demo.tex demo.bib
-cp .latex-cache/demo.pdf /Users/rdube/Tmp/demo.pdf
--- presentations/shellcmd/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-latexmk -xelatex -output-directory=.latex-cache cmd.tex cmd.bib
-cp .latex-cache/cmd.pdf /Users/rdube/Tmp/cmd.pdf
